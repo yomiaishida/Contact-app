d518a69815723f45b0e067578ae4efd7785f0ae8
Abayomi Aishida <yomicorperate@gmail.com>
2020-04-05 23:00:56 +0100

    changed browserRouter to HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./coomponents/contacts/Contacts";
 import AddContact from "./coomponents/contacts/AddContact";
 import EditContact from "./coomponents/contacts/EditContact";
