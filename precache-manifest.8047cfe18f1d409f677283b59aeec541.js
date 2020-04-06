19edec7be327eeaa3e1e0292f6ec1f3b1ef4a09a
Abayomi Aishida <yomicorperate@gmail.com>
2020-04-06 17:48:01 +0100

    Updates

---
+++ precache-manifest.8047cfe18f1d409f677283b59aeec541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8246b8ca926d066fc92cc9a0639a6965",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "270558036812b5d18dd0",
+    "url": "/contact-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5c39a73db37d557d39bf",
+    "url": "/contact-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "270558036812b5d18dd0",
+    "url": "/contact-app/static/js/2.9a9dfa3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-app/static/js/2.9a9dfa3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c39a73db37d557d39bf",
+    "url": "/contact-app/static/js/main.11499487.chunk.js"
+  },
+  {
+    "revision": "b0acdc0c0d2ed88418be",
+    "url": "/contact-app/static/js/runtime-main.741f9cd8.js"
+  }
+]);
